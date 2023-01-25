65bd3d3d534a4d056e2a31dc6b26ba690751dc5f
Elliot Jackson <13633636+ElliotMJackson@users.noreply.github.com>
2023-01-25 17:49:14 -0500

    add landing page

--- sidebars.js
+++ sidebars.js
@@ -3,117 +3,130 @@
 const sidebars = {
   docs: [
     "introduction",
+    "about",
     "installation",
     {
       type: "category",
-      label: "The Buf CLI",
+      label: "Guides",
       items: [
-        { type: "doc", label: "Breaking Change Detection", id: "buf/breaking/explanation" },
-        { type: "doc", label: "Build", id: "buf/build/explanation" },
-        { type: "doc", label: "Convert", id: "buf/convert/explanation" },
-        { type: "doc", label: "cURL", id: "buf/curl/explanation" },
-        { type: "doc", label: "Export", id: "buf/export/explanation" },
-        { type: "doc", label: "Format", id: "buf/format/explanation" },
-        { type: "doc", label: "Generate", id: "buf/generate/explanation" },
-        { type: "doc", label: "Lint", id: "buf/lint/explanation" },
-        { type: "doc", label: "Mod", id: "buf/mod/explanation" },
-        { type: "doc", label: "Push", id: "buf/push/explanation" },
-        { type: "doc", label: "Registry", id: "buf/registry/explanation" },
         {
           type: "category",
-          label: "Best Practices",
+          label: "Tutorials",
           items: [
-            "buf/best-practices/module-development",
-            "buf/best-practices/style-guide"
+            {
+              type: "doc",
+              id: "tutorials/getting-started/getting-started-with-buf",
+              label: "Getting started with Buf"
+            },
+            {
+              type: "doc",
+              id: "tutorials/going-further/going-further-with-buf",
+              label: "Going further with Buf"
+            },
+            {
+              type: "doc",
+              id: "tutorials/getting-started/getting-started-with-bsr",
+              label: "Getting started with the BSR"
+            },
+            {
+              type: "doc",
+              id: "tutorials/going-further/going-further-with-bsr",
+              label: "Going further with the BSR"
+            },
+            {
+              type: "doc",
+              id: "tutorials/getting-started/getting-started-with-buf-studio",
+              label: "Getting Started with Buf Studio"
+            }
           ],
           collapsed: false
         },
         {
           type: "category",
-          label: "Other",
+          label: "How To",
           items: [
-            "buf/other/inputs",
-            "buf/other/internal-compiler",
-            "buf/other/protoc-plugins",
-            "buf/other/workspaces",
-            "buf/other/bazel"
+            "how-to/how-to-use-buf-in-your-js-project",
+            "how-to/build-an-api-client-with-buf",
+            "how-to/replace-protoc-with-buf",
+            "how-to/iterate-on-modules",
+            "how-to/migrate-from-protolock",
+            "how-to/migrate-from-prototool",
+            "buf/breaking/how-to",
+            "buf/build/how-to",
+            "buf/generate/how-to",
+            "bsr/remote-packages/how-to",
+            {
+              type: "doc",
+              id: "ci-cd/how-to",
+              label: "Buf on GitHub Actions: A Setup Guide"
+            }
           ],
           collapsed: false
         }
-      ],
-      collapsed: false
+      ]
     },
     {
       type: "category",
-      label: "The Buf Schema Registry (BSR)",
-      items: [
-        "bsr/introduction",
-        "bsr/explanation",
-        { type: "doc", label: "Documentation", id: "bsr/documentation/explanation" },
-        { type: "doc", label: "Authentication", id: "bsr/authentication/explanation" },
-        { type: "doc", label: "Dependency Management", id: "bsr/dependency-management/explanation" },
-        { type: "doc", label: "Remote Packages", id: "bsr/remote-packages/explanation" },
-        { type: "doc", label: "Remote Plugins", id: "bsr/remote-plugins/explanation" },
-        { type: "doc", label: "Schema Management", id: "bsr/schema-management/explanation" },
-        { type: "doc", label: "Studio", id: "bsr/studio/explanation" },
-        { type: "doc", label: "User Management", id: "bsr/user-management/explanation" },
-        { type: "category", label: "Data Model", items: ["bsr/data-model/images"], collapsed: false }
-      ],
-      collapsed: false
-    },
-    {
-      type: "category",
-      label: "Tutorials",
+      label: "Manuals",
       items: [
         {
-          type: "doc",
-          id: "tutorials/getting-started/getting-started-with-buf",
-          label: "Getting started with Buf"
+          type: "category",
+          label: "The Buf CLI",
+          items: [
+            { type: "doc", label: "Breaking Change Detection", id: "buf/breaking/explanation" },
+            { type: "doc", label: "Build", id: "buf/build/explanation" },
+            { type: "doc", label: "Convert", id: "buf/convert/explanation" },
+            { type: "doc", label: "cURL", id: "buf/curl/explanation" },
+            { type: "doc", label: "Export", id: "buf/export/explanation" },
+            { type: "doc", label: "Format", id: "buf/format/explanation" },
+            { type: "doc", label: "Generate", id: "buf/generate/explanation" },
+            { type: "doc", label: "Lint", id: "buf/lint/explanation" },
+            { type: "doc", label: "Mod", id: "buf/mod/explanation" },
+            { type: "doc", label: "Push", id: "buf/push/explanation" },
+            { type: "doc", label: "Registry", id: "buf/registry/explanation" },
+            {
+              type: "category",
+              label: "Best Practices",
+              items: [
+                "buf/best-practices/module-development",
+                "buf/best-practices/style-guide"
+              ],
+              collapsed: false
+            },
+            {
+              type: "category",
+              label: "Other",
+              items: [
+                "buf/other/inputs",
+                "buf/other/internal-compiler",
+                "buf/other/protoc-plugins",
+                "buf/other/workspaces",
+                "buf/other/bazel"
+              ],
+              collapsed: false
+            }
+          ],
+          collapsed: false
         },
         {
-          type: "doc",
-          id: "tutorials/going-further/going-further-with-buf",
-          label: "Going further with Buf"
-        },
-        {
-          type: "doc",
-          id: "tutorials/getting-started/getting-started-with-bsr",
-          label: "Getting started with the BSR"
-        },
-        {
-          type: "doc",
-          id: "tutorials/going-further/going-further-with-bsr",
-          label: "Going further with the BSR"
-        },
-        {
-          type: "doc",
-          id: "tutorials/getting-started/getting-started-with-buf-studio",
-          label: "Getting Started with Buf Studio"
+          type: "category",
+          label: "The Buf Schema Registry (BSR)",
+          items: [
+            "bsr/introduction",
+            "bsr/explanation",
+            { type: "doc", label: "Documentation", id: "bsr/documentation/explanation" },
+            { type: "doc", label: "Authentication", id: "bsr/authentication/explanation" },
+            { type: "doc", label: "Dependency Management", id: "bsr/dependency-management/explanation" },
+            { type: "doc", label: "Remote Packages", id: "bsr/remote-packages/explanation" },
+            { type: "doc", label: "Remote Plugins", id: "bsr/remote-plugins/explanation" },
+            { type: "doc", label: "Schema Management", id: "bsr/schema-management/explanation" },
+            { type: "doc", label: "Studio", id: "bsr/studio/explanation" },
+            { type: "doc", label: "User Management", id: "bsr/user-management/explanation" },
+            { type: "category", label: "Data Model", items: ["bsr/data-model/images"], collapsed: false }
+          ],
+          collapsed: false
         }
-      ],
-      collapsed: false
-    },
-    {
-      type: "category",
-      label: "How To",
-      items: [
-        "how-to/how-to-use-buf-in-your-js-project",
-        "how-to/build-an-api-client-with-buf",
-        "how-to/replace-protoc-with-buf",
-        "how-to/iterate-on-modules",
-        "how-to/migrate-from-protolock",
-        "how-to/migrate-from-prototool",
-        "buf/breaking/how-to",
-        "buf/build/how-to",
-        "buf/generate/how-to",
-        "bsr/remote-packages/how-to",
-        {
-          type: "doc",
-          id: "ci-cd/how-to",
-          label: "Buf on GitHub Actions: A Setup Guide"
-        }
-      ],
-      collapsed: false
+      ]
     },
     {
       type: "category",
@@ -198,9 +211,7 @@ const sidebars = {
           collapsed: true
         }
       ],
-      collapsed: false
     }
-
   ]
 };
 
